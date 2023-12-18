cb7249d835ee4cc353bac83fdd60979f3c42f2e9
diagui <tounkarakan@gmail.com>
2023-12-18 12:41:16 +0300

    Changing Colors and Fonts

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,10 @@ class AdminPanelProvider extends PanelProvider
             ->path('dashboard')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
+            ->font('Outfit')
+            ->favicon('images/logoWg.png')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,2 @@
+{{-- <img src="https://avatars.githubusercontent.com/u/101643951?s=48&v=4" alt="My Github Profile"> --}}
+<img src="{{asset('images/myOfficialLogo.png')}}" alt="My Github Profile" style="margin-top: 30px;">
